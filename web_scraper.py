41dc76f1b69e59a6e3c22e20c44278ef4ee88e6b
James Widmer <jow257@pitt.edu>
2026-07-14 16:05:29 -0400

    barebones working script

---
+++ web_scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+def scrape(url, directory):
+    url=url+"/sets"
+    # Get the web page content
+    response = requests.get(url)
+
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    #get the user's profile ending
+    user_url = url[22:]
+    
+    #Find and print all the links
+    for link in soup.find_all('a'):
+        link_string=link.get('href')
+        if user_url in link_string and user_url!=link_string:
+            print(link_string[link_string.index("sets/")+5:] + " ",end="")
+            print(url[:22]+link_string + " ", end="")
+
