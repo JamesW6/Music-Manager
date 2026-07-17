cc0ab488f14f9a8b7bd3300486ff5377b8619b4e
James Widmer <jow257@pitt.edu>
2026-07-17 09:33:11 -0400

    adds support for downloading youtube playlists

--- sc_scrape.py
+++ sc_scrape.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 
-def scrape(url, directory):
+def scrape_soundcloud():
+    url = sys.argv[1]
     url=url+"/sets"
     # Get the web page content
     response = requests.get(url)
-
+ 
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -16,6 +18,8 @@ def scrape(url, directory):
     for link in soup.find_all('a'):
         link_string=link.get('href')
         if user_url in link_string and user_url!=link_string:
-            print(link_string[link_string.index("sets/")+5:] + " ",end="")
+            print(link_string[link_string.index("sets/")+5:].replace(" ","_") + " ",end="")
             print(url[:22]+link_string + " ", end="")
 
+
+scrape_soundcloud()
---
+++ yt_scrape.py
@@ -0,0 +1,28 @@
+
+# Source - https://stackoverflow.com/a/35905689
+# Posted by Florent B., modified by community. See post 'Timeline' for change history
+# Retrieved 2026-07-16, License - CC BY-SA 3.0
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import sys
+import time
+
+def scrape_yt(): 
+    url=sys.argv[1]
+    url+="/playlists"
+    driver = webdriver.Firefox()
+    driver.get(url)
+    
+    time.sleep(5)
+    links=driver.find_elements(By.TAG_NAME, "a")
+    
+    for playlist in links:
+        link=playlist.get_attribute("href")
+        if type(link)!=type(None) and  "/playlist?" in link: 
+            print(playlist.text.replace(" ","_")+" ",end="")
+            print(link+" ",end="")
+scrape_yt()
+
+
